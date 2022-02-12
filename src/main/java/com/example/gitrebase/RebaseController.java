2ab02e5e7e734cf6616af5de95ebfead75f1ee7c
ArturMkrtumyan <52348327+ArturMkrtumyan@users.noreply.github.com>
2022-02-12 20:05:27 +0400

    Update RebaseController.java

--- src/main/java/com/example/gitrebase/RebaseController.java
+++ src/main/java/com/example/gitrebase/RebaseController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class RebaseController {
     @GetMapping
     public String message(){
-        System.out.printf("added by User");
+        System.out.printf("added by Admin");
         return "rebase example by User";
     }
 }
